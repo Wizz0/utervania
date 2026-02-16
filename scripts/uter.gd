846bf5b463807426f79c30c2298400c72706398d
Semyon Gorokhov <samgorokhov01@gmail.com>
2026-02-16 13:01:24 +0900

    added camera shaking on death

--- scripts/uter.gd
+++ scripts/uter.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 class_name Uter
 
+@onready var camera: Camera2D = $Camera2D
+
 @export var speed: float = 200.0
 @export var jump_force: float = 200.0
 @export var dash_force: float = 550.0
@@ -13,6 +15,7 @@ var checkpoint: Vector2
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 func die():
+	camera.screen_shake(5, 0.2)
 	position = checkpoint
 
 func save(new_checkpoint):
