1c0d99fa267b5c11f240becdf0124d559deb10eb
Semyon Gorokhov <samgorokhov01@gmail.com>
2026-02-18 09:36:47 +0900

    added timer

--- scripts/uter.gd
+++ scripts/uter.gd
@@ -13,11 +13,14 @@ var is_dash_used: bool = false
 var checkpoint: Vector2
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer_label: Label = $HUD/Timer
 
 @export var unlock_all_abilities: bool = false
 
 var abilities: Dictionary = {}
 var watermelons: int = 0
+
+var elapsed_time: float = 0.0
 
 func _ready() -> void:
 	if unlock_all_abilities:
@@ -25,6 +28,12 @@ func _ready() -> void:
 			"double_jump": true,
 			"dash": true
 		}
+	
+	start_timer()
+
+func _process(delta: float) -> void:
+	elapsed_time += delta
+	update_timer_display()
 
 func die():
 	camera.screen_shake(5, 0.2)
@@ -47,3 +56,13 @@ func add_ability(ability: String):
 func add_watermelon(num: int = 1):
 	watermelons += num
 	print(watermelons)
+
+func start_timer():
+	elapsed_time = 0.0
+	update_timer_display()
+
+func update_timer_display():
+	var minutes = int(elapsed_time / 60)
+	var seconds = int(elapsed_time) % 60
+	var milliseconds = int((elapsed_time - int(elapsed_time)) * 100)
+	timer_label.text = "%d:%02d:%02d" % [minutes, seconds, milliseconds]
